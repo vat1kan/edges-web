d7acbfdcff39c11a1c872e8494a340b286c0c942
@vat1kan <gariksgs044@gmail.com>
2025-05-08 16:58:13 +0300

    option of preloaded bsds500 data set

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const checkbox = document.getElementById('toggle-switch');
+    const uploadBlock = document.getElementById('uploadBlock');
+    const fileInput = document.getElementById('fileInput');
+  
+    function toggleUploadBlock() {
+      if (checkbox.checked) {
+        uploadBlock.classList.add('d-none');
+        fileInput.removeAttribute('required');
+      } else {
+        uploadBlock.classList.remove('d-none');
+        fileInput.setAttribute('required', 'required');
+      }
+    }
+  
+    checkbox.addEventListener('change', toggleUploadBlock);
+  
+    toggleUploadBlock();
+});
+  
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleSwitch = document.getElementById("toggle-switch");
+    const metricsCheckbox = document.getElementById("calculate_metrics");
+
+    toggleSwitch.addEventListener("change", toggleGroundTruthField);
+    metricsCheckbox.addEventListener("change", toggleGroundTruthField);
+
+    toggleGroundTruthField();
+});
+
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    document.getElementById("noise_check").addEventListener("change", toggleNoiseFields);
+
+    document.getElementById("noise_type").addEventListener("change", function() 
+    {
+        document.getElementById("noise_value").value = 0;
+        toggleNoiseFields();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    var accordion = document.getElementById("detailsButton");
+    accordion.addEventListener("click", function() 
+    {
+        this.classList.toggle("active");
+        var panel = document.getElementById("errorDetails");
+        if (panel.style.display === "block") 
+        {
+            panel.style.display = "none";
+        } 
+        else 
+        {
+            panel.style.display = "block";
+        }
+    });
+});
+
 function toggleNoiseFields() 
 {
     var checkbox = document.getElementById("noise_check");
@@ -18,86 +78,71 @@ function toggleNoiseFields()
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() 
-{
-    document.getElementById("noise_check").addEventListener("change", toggleNoiseFields);
+function toggleGroundTruthField() {
+    const metricsCheck = document.getElementById("calculate_metrics");
+    const metricsFields = document.getElementById('metrics_fields');
+    const groundTruthInput = document.getElementsByName("ground_truth")[0];
+    const infoMessage = document.getElementById('bsds-message');
+    const useBsds = document.getElementById("toggle-switch").checked;
 
-    document.getElementById("noise_type").addEventListener("change", function() 
-    {
-        document.getElementById("noise_value").value = 0;
-        toggleNoiseFields();
-    });
-});
-
-
-function toggleGroundTruthField() 
-{
-    var metricsCheck = document.getElementById("calculate_metrics");
-    var metricsFields = document.getElementById('metrics_fields');
-    var groundTruthInput = document.getElementsByName("ground_truth")[0];
-
-    if (metricsCheck.checked) 
+    if (metricsCheck.checked && !useBsds) 
     {
         metricsFields.style.display = 'block';
         groundTruthInput.required = true;
+        infoMessage.hidden = true;
+    } 
+    else if (metricsCheck.checked && useBsds) 
+    {
+        metricsFields.style.display = 'none';
+        groundTruthInput.required = false;
+        infoMessage.hidden = false;
     } 
     else 
     {
         metricsFields.style.display = 'none';
         groundTruthInput.required = false;
+        infoMessage.hidden = true;
     }
 }
 
 function validateForm() {
-    var metricsCheck = document.getElementById("calculate_metrics");
-    var mainFilesInput = document.getElementsByName("file")[0];
-    var mainFiles = mainFilesInput.files;
-    var gtFilesInput = document.getElementsByName("ground_truth")[0];
-    var gtFiles = gtFilesInput.files;
+    const useBsds = document.getElementById("toggle-switch").checked;
+    const metricsCheck = document.getElementById("calculate_metrics").checked;
 
-    if (mainFiles.length === 0) {
-        alert("Please upload at least one image.");
-        return false;
-    }
+    const mainFilesInput = document.getElementsByName("file")[0];
+    const mainFiles = mainFilesInput.files;
 
-    if (mainFiles.length > 30) {
-        alert("You can upload at most 30 images.");
-        return false;
-    }
+    const gtFilesInput = document.getElementsByName("ground_truth")[0];
+    const gtFiles = gtFilesInput.files;
 
-    for (var i = 0; i < mainFiles.length; i++) {
-        if (mainFiles[i].size > 5 * 1024 * 1024 || (gtFiles[i] && gtFiles[i].size > 5 * 1024 * 1024)) {
-            alert("Image size should be less than 5 MB.");
+    if (!useBsds) {
+        if (mainFiles.length === 0) {
+            alert("Please upload at least one image.");
             return false;
+        }
+
+        if (mainFiles.length > 10) {
+            alert("You can upload at most 10 images.");
+            return false;
+        }
+
+        for (let i = 0; i < mainFiles.length; i++) {
+            if (
+                mainFiles[i].size > 1 * 1024 * 1024 ||
+                (gtFiles[i] && gtFiles[i].size > 1 * 1024 * 1024)
+            ) {
+                alert("Image size should be less than 1 MB.");
+                return false;
+            }
+        }
+
+        if (metricsCheck) {
+            if (mainFiles.length !== gtFiles.length) {
+                alert("Number of original images must match number of ground truth images.");
+                return false;
+            }
         }
     }
 
-    if (metricsCheck.checked) {
-        var groundTruthFiles = document.getElementsByName("ground_truth")[0].files;
-        console.log(mainFiles.length, groundTruthFiles.length);
-        if (mainFiles.length !== groundTruthFiles.length) {
-            alert("Number of original images must match number of ground truth images.");
-            return false;
-        }
-        return true;
-    }
+    return true;
 }
-
-document.addEventListener('DOMContentLoaded', function() 
-{
-    var accordion = document.getElementById("detailsButton");
-    accordion.addEventListener("click", function() 
-    {
-        this.classList.toggle("active");
-        var panel = document.getElementById("errorDetails");
-        if (panel.style.display === "block") 
-        {
-            panel.style.display = "none";
-        } 
-        else 
-        {
-            panel.style.display = "block";
-        }
-    });
-});
-
